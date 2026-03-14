ca4ce5ce2cb12b1b38dfd47dd80f4ef43e29e0e2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-14 19:52:07 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/status_update_pages/damaged_decommission/damaged_decommission_widget.dart
+++ lib/pages/status_update_pages/damaged_decommission/damaged_decommission_widget.dart
@@ -9,6 +9,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
 import '/custom_code/actions/index.dart' as actions;
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -68,14 +69,6 @@ class _DamagedDecommissionWidgetState extends State<DamagedDecommissionWidget> {
 
     _model.enterSSCCTextController ??= TextEditingController();
     _model.enterSSCCFocusNode ??= FocusNode();
-    _model.enterSSCCFocusNode!.addListener(
-      () async {
-        await _model.checkSerialStatus(
-          context,
-          serial: _model.enterSSCCTextController.text,
-        );
-      },
-    );
   }
 
   @override
@@ -245,6 +238,19 @@ class _DamagedDecommissionWidgetState extends State<DamagedDecommissionWidget> {
                                           controller:
                                               _model.enterSSCCTextController,
                                           focusNode: _model.enterSSCCFocusNode,
+                                          onChanged: (_) =>
+                                              EasyDebounce.debounce(
+                                            '_model.enterSSCCTextController',
+                                            Duration(milliseconds: 2000),
+                                            () async {
+                                              await _model.checkSerialStatus(
+                                                context,
+                                                serial: _model
+                                                    .enterSSCCTextController
+                                                    .text,
+                                              );
+                                            },
+                                          ),
                                           onFieldSubmitted: (_) async {
                                             await _model.checkSerialStatus(
                                               context,
