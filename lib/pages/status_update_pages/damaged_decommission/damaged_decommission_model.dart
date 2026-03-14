c7721dacd91f688683f0813f2caeb68af925928d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-14 19:41:59 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/status_update_pages/damaged_decommission/damaged_decommission_model.dart
+++ lib/pages/status_update_pages/damaged_decommission/damaged_decommission_model.dart
@@ -2,6 +2,7 @@ import '/backend/api_requests/api_calls.dart';
 import '/components/empty_list_view_display/empty_list_view_display_widget.dart';
 import '/components/loading_widget.dart';
 import '/components/scan_button/scan_button_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/form_field_controller.dart';
 import '/custom_code/actions/index.dart' as actions;
@@ -106,7 +107,21 @@ class DamagedDecommissionModel
 
       if ((checkSerialStatusApiResult.succeeded ?? true)) {
         addToScannedSerialToDecommission(enterSSCCTextController.text);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Faild To Connect The Server',
+              style: TextStyle(
+                color: Color(0xFFDDDDDD),
+              ),
+            ),
+            duration: Duration(milliseconds: 4000),
+            backgroundColor: FlutterFlowTheme.of(context).error,
+          ),
+        );
       }
+
       loading = false;
     }
   }
